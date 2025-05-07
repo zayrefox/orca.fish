c1cba1fc658e4f08ee933273fe5dcbd485bfc8d8
Gladis Orca <140742009+zayrefox@users.noreply.github.com>
2025-05-06 20:44:26 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+new Swiper(".wrapper", {
+    loop: true,
+    spaceBetween: 30,
+    // Autoplay
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    // Pagination bullets
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
