cedcdc1ba9fe6cd103c6a2d771c5d8889fc8eede
Zayre Fox <zayrefox@gmail.com>
2025-07-17 12:34:45 -0400

    Add new page

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php
+<?php $id=$_GET['id']; 
+ echo '<img src="'.$id.'.jpg"/>';
 
-$image = file_get_contents('https://zayrefox.github.io/orca.fish/assets/headshot.jpg');
-
-header('Content-type: image/jpeg;');
-header("Content-Length: " . strlen($image));
-echo $image;
+?>
